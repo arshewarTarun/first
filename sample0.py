8c9b69f6b93dcb04f0b1b7c9c31505a3c861804c
Tarun Arshewar <tarun.arshewar@modak.com>
2025-09-24 17:29:11 +0530

    Update sample0.py

--- sample0.py
+++ sample0.py
@@ -2,27 +2,21 @@ from pyspark.sql import SparkSession
 import sys
 
 try:
-    # Initialize Spark session
     spark = SparkSession.builder \
         .appName("S3 to S3 Data Move") \
+        .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem") \
         .getOrCreate()
 
-    # S3 paths
-    input_path = "s3://cdpmodakbucket/ta22_test/COLUMN.csv"
-    #output_path = "s3a://j-and-j-test-bucket/target_combined_new_up"
+    input_path = "s3a://cdpmodakbucket/ta22_test/COLUMN.csv"
 
-    # Read Parquet file
     print(f"Reading from: {input_path}")
-    df = spark.read.csv(input_path)
+    df = spark.read.option("header", "true").csv(input_path)
     df.show()
-
-
 
 except Exception as e:
     print(f"An error occurred: {str(e)}")
     sys.exit(1)
 
 finally:
-    # Stop the Spark session
     spark.stop()
     print("Spark session stopped.")
