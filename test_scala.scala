ea5503474aa073bb4ab5d88f81e117c2fa129a3d
Tarun Arshewar <tarun.arshewar@modak.com>
2025-12-11 11:49:04 +0530

    Update test_scala.scala

--- test_scala.scala
+++ test_scala.scala
@@ -3,103 +3,104 @@
 //adding new line july 11
 //adding new line july-10
 
-import org.apache.spark.sql.{SparkSession, DataFrame}
+// import org.apache.spark.sql.{SparkSession, DataFrame}
 
-import org.apache.spark.sql.types.StructType
+// import org.apache.spark.sql.types.StructType
  
-object PostgresParquetComparator {
+// object PostgresParquetComparator {
  
-  def main(args: Array[String]): Unit = {
+//   def main(args: Array[String]): Unit = {
  
-    val spark = SparkSession.builder()
+//     val spark = SparkSession.builder()
 
-      .appName("Compare Postgres and Parquet")
+//       .appName("Compare Postgres and Parquet")
 
-      .master("local[*]") // remove or change this for cluster mode
+//       .master("local[*]") // remove or change this for cluster mode
 
-      .getOrCreate()
-   //s3 connection config
+//       .getOrCreate()
+//    //s3 connection config
    
-    spark.sparkContext.hadoopConfiguration.set("fs.s3a.access.key", "ACCESS_KEY")
+//     spark.sparkContext.hadoopConfiguration.set("fs.s3a.access.key", "ACCESS_KEY")
    
-    spark.sparkContext.hadoopConfiguration.set("fs.s3a.secret.key", "SECRET_KEY")
+//     spark.sparkContext.hadoopConfiguration.set("fs.s3a.secret.key", "SECRET_KEY")
    
-    spark.sparkContext.hadoopConfiguration.set("fs.s3a.endpoint", "s3.amazonaws.com")
+//     spark.sparkContext.hadoopConfiguration.set("fs.s3a.endpoint", "s3.amazonaws.com")
    
-    spark.sparkContext.hadoopConfiguration.set("fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
+//     spark.sparkContext.hadoopConfiguration.set("fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
  
-    // PostgreSQL connection config
+//     // PostgreSQL connection config
 
-    val pgUrl = "URL"
+//     val pgUrl = "URL"
 
-    val pgTable = "TABLE"
+//     val pgTable = "TABLE"
 
-    val pgUser = "USER"
+//     val pgUser = "USER"
 
-    val pgPassword = "PASSWORD"
+//     val pgPassword = "PASSWORD"
  
-    // Path to the Parquet file
+//     // Path to the Parquet file
 
-    val parquetPath = "PATH"
+//     val parquetPath = "PATH"
  
-    // Read PostgreSQL table
+//     // Read PostgreSQL table
 
-    val pgDF: DataFrame = spark.read
+//     val pgDF: DataFrame = spark.read
 
-      .format("jdbc")
+//       .format("jdbc")
 
-      .option("url", pgUrl)
+//       .option("url", pgUrl)
 
-      .option("dbtable", pgTable)
+//       .option("dbtable", pgTable)
 
-      .option("user", pgUser)
+//       .option("user", pgUser)
 
-      .option("password", pgPassword)
+//       .option("password", pgPassword)
 
-      .option("driver", "org.postgresql.Driver")
+//       .option("driver", "org.postgresql.Driver")
 
-      .load()
+//       .load()
  
-    // Read Parquet file
+//     // Read Parquet file
 
-    val parquetDF: DataFrame = spark.read.parquet(parquetPath)
+//     val parquetDF: DataFrame = spark.read.parquet(parquetPath)
  
-    // Compare row counts
+//     // Compare row counts
 
-    val pgCount = pgDF.count()
+//     val pgCount = pgDF.count()
 
-    val parquetCount = parquetDF.count()
+//     val parquetCount = parquetDF.count()
  
-    println(s"PostgreSQL row count: $pgCount")
+//     println(s"PostgreSQL row count: $pgCount")
 
-    println(s"Parquet row count   : $parquetCount")
+//     println(s"Parquet row count   : $parquetCount")
  
-    // Compare schemas
+//     // Compare schemas
 
-    val pgSchema: StructType = pgDF.schema
+//     val pgSchema: StructType = pgDF.schema
 
-    val parquetSchema: StructType = parquetDF.schema
+//     val parquetSchema: StructType = parquetDF.schema
  
-    val schemaEqual = pgSchema.equals(parquetSchema)
+//     val schemaEqual = pgSchema.equals(parquetSchema)
 
-    println(s"Schemas are equal: $schemaEqual")
+//     println(s"Schemas are equal: $schemaEqual")
  
-    if (!schemaEqual) {
+//     if (!schemaEqual) {
 
-      println("PostgreSQL Schema:")
+//       println("PostgreSQL Schema:")
 
-      pgSchema.printTreeString()
+//       pgSchema.printTreeString()
  
-      println("Parquet Schema:")
+//       println("Parquet Schema:")
 
-      parquetSchema.printTreeString()
+//       parquetSchema.printTreeString()
 
-    }
+//     }
  
-    spark.stop()
+//     spark.stop()
 
-  }
+//   }
 
-}
-
- 
+// }
+import com.jcraft.jsch.JSch
+val jsch = new com.jcraft.jsch.JSch()
+println(s"JSch Version: ${com.jcraft.jsch.JSch.VERSION}")
